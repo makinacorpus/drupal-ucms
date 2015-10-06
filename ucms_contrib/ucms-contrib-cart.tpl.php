3722ecc034f67aed670a17e8019317fcde1ed7e5
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-10-06 18:21:17 +0200

    A little bit of Drupal default theme theming for cart

--- ucms_contrib/ucms-contrib-cart.tpl.php
+++ ucms_contrib/ucms-contrib-cart.tpl.php
@@ -1,9 +1,15 @@
 <div id="ucms-cart">
   <h2><?php echo t("Your favorites"); ?></h2>
-  <?php foreach ($items as $nid => $item): ?>
-    <div class="ucms-cart-item" data-nid="<?php echo render($nid); ?>">
-      <?php echo render($item); ?>
-    </div>
-  <?php endforeach; ?>
-  <a id="ucms-cart-trash" href="#"><?php echo t("Trash"); ?></a>
+  <?php echo render($actions); ?>
+  <div>
+    <?php foreach ($items as $nid => $item): ?>
+      <div class="ucms-cart-item" data-nid="<?php echo render($nid); ?>">
+        <?php echo render($item); ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <div class="bottom">
+    <a id="ucms-cart-toggle" href="#"><?php echo t("Close pane"); ?></a>
+    <a id="ucms-cart-trash" href="#"><?php echo t("Trash"); ?></a>
+  </div>
 </div>
