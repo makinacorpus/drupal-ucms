180b0507132497a06aa6966b9a93b5b9565fd84c
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-01-07 19:08:26 +0100

    Node grants, site init and site theme should now be OK

--- ucms_site/src/Access.php
+++ ucms_site/src/Access.php
@@ -8,6 +8,21 @@ namespace MakinaCorpus\Ucms\Site;
 final class Access
 {
     /**
+     * Default group identifier for grants where it does not make sense.
+     */
+    const GID_DEFAULT = 1;
+
+    /**
+     * Default priority for grants.
+     */
+    const PRIORITY_DEFAULT = 1;
+
+    /**
+     * Default realm for access checks.
+     */
+    const REALM_PUBLIC = 'ucms_public';
+
+    /**
      * Default realm for access checks.
      */
     const REALM_SITE = 'ucms_site';
@@ -16,6 +31,11 @@ final class Access
      * Default realm for access checks.
      */
     const REALM_GLOBAL = 'ucms_global';
+
+    /**
+     * Default realm for access checks.
+     */
+    const REALM_GLOBAL_LABELED = 'ucms_global_labeled';
 
     /**
      * View operation.
@@ -33,9 +53,49 @@ final class Access
     const OP_DELETE = 'delete';
 
     /**
+     * User can view global labeled content permission.
+     */
+    const PERM_GLOBAL_LABELED_VIEW = 'view global labeled content';
+
+    /**
+     * User can view global content permission.
+     */
+    const PERM_GLOBAL_VIEW = 'view global content';
+
+    /**
+     * User can edit global labeled content permission.
+     */
+    const PERM_GLOBAL_LABELED_EDIT = 'edit global labeled content';
+
+    /**
+     * User can edit global content permission.
+     */
+    const PERM_GLOBAL_EDIT = 'edit global content';
+
+    /**
+     * Functional administrator role.
+     */
+    const ROLE_ADMIN_FUNC = 'admin_func';
+
+    /**
+     * Technical administrator role.
+     */
+    const ROLE_ADMIN_TECH = 'admin_tech';
+
+    /**
+     * User accounts administrator role.
+     */
+    const ROLE_ADMIN_USER = 'admin_user';
+
+    /**
      * Internet visitor role.
      */
     const ROLE_ANONYMOUS = 'anonymous';
+
+    /**
+     * Content moderator.
+     */
+    const ROLE_MODERATOR = 'moderator';
 
     /**
      * Global database webmaster.
