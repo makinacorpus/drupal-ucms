e30089536b0e7169dbd83ba9dc906acb3e8f8a8d
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-01-13 15:45:04 +0100

    contrib: added the CartStorage class to replace some procedural code

---
+++ ucms_contrib/src/CartStorage.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Contrib;
+
+class CartStorage
+{
+    /**
+     * @var \DatabaseConnection
+     */
+    private $db;
+
+    /**
+     * Default constructor
+     *
+     * @param \DatabaseConnection $db
+     */
+    public function __construct(\DatabaseConnection $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Add content to favorites
+     *
+     * @param int $uid
+     * @param int $nid
+     *
+     * @return boolean
+     */
+    public function addFor($uid, $nid)
+    {
+        $exists = (bool)$this
+            ->db
+            ->query("SELECT 1 FROM {ucms_contrib_cart} WHERE nid = :nid AND uid = :uid", [
+                ':nid' => $nid,
+                ':uid' => $uid,
+            ])
+            ->fetchField()
+        ;
+
+        if ($exists) {
+            return false;
+        }
+
+        $this
+            ->db
+            ->merge('ucms_contrib_cart')
+            ->key([
+                'nid' => $nid,
+                'uid' => $uid,
+            ])
+            ->execute()
+        ;
+
+        return true;
+    }
+
+    /**
+     * Remove content from favorites
+     *
+     * @param int $uid
+     * @param int $nid
+     */
+    public function removeFor($uid, $nid)
+    {
+        $this
+            ->db
+            ->delete('ucms_contrib_cart')
+            ->condition('nid', $nid)
+            ->condition('uid', $uid)
+            ->execute()
+        ;
+    }
+
+    /**
+     * List favorite content
+     *
+     * @param int $uid
+     *
+     * @return int[]
+     *   Ordered list of node identifiers.
+     */
+    public function listFor($uid)
+    {
+        $q = $this
+            ->db
+            ->select('ucms_contrib_cart', 'c')
+            ->fields('c', ['nid'])
+            ->condition('c.uid', $uid)
+        ;
+
+        $q->join('node', 'n', "n.nid = c.nid");
+
+        return $q
+            //->extend('PagerDefault')
+            //->limit(12)
+            ->addTag('node_access')
+            ->execute()
+            ->fetchCol()
+        ;
+    }
+}
