8b8f326b5d23dc3d583fcbcda653576380ace836
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-01-29 13:18:18 +0100

    dashboard, site: fixed a few PHP warnings due to typos

--- ucms_site/src/Page/SiteAdminDisplay.php
+++ ucms_site/src/Page/SiteAdminDisplay.php
@@ -71,7 +71,7 @@ class SiteAdminDisplay extends AbstractDisplay
                         check_plain($states[$site->state]),
                         format_interval($site->ts_created->getTimestamp()),
                         format_interval($site->ts_changed->getTimestamp()),
-                        isset($accountMap[$site]->uid) ? format_username($accountMap[$site->uid]) : '',
+                        isset($accountMap[$site->uid]) ? format_username($accountMap[$site->uid]) : '',
                         theme('ucms_dashboard_actions', ['actions' => $this->getActions($site), 'mode' => 'icon']),
                     ];
                 }
