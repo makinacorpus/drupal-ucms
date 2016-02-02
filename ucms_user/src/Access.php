9e83524df18534c08ab65989bd92e7ab325a8025
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-02 19:25:21 +0100

    user: added skeletong module for user management feature

---
+++ ucms_user/src/Access.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MakinaCorpus\Ucms\User;
+
+/**
+ * Access constants
+ */
+final class Access
+{
+    /**
+     * Access user dashboard permission
+     */
+    const PERM_USER_MANAGE_ALL = 'user manage all';
+}
