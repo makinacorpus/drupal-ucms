39e19a1b4542619511e132283b21ff726266312f
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-03 14:17:56 +0100

    site: implemented detail and log views

--- ucms_site/src/EventDispatcher/ContextPaneEventListener.php
+++ ucms_site/src/EventDispatcher/ContextPaneEventListener.php
@@ -5,11 +5,27 @@ namespace MakinaCorpus\Ucms\Site\EventDispatcher;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 use MakinaCorpus\Ucms\Dashboard\Action\Action;
+use MakinaCorpus\Ucms\Dashboard\Action\ActionRegistry;
 use MakinaCorpus\Ucms\Dashboard\EventDispatcher\ContextPaneEvent;
 
 class ContextPaneEventListener
 {
     use StringTranslationTrait;
+
+    /**
+     * @var ActionRegistry
+     */
+    private $actionRegistry;
+
+    /**
+     * Default constructor
+     *
+     * @param ActionRegistry $actionRegistry
+     */
+    public function __construct(ActionRegistry $actionRegistry)
+    {
+        $this->actionRegistry = $actionRegistry;
+    }
 
     public function onUcmsdashboardContextinit(ContextPaneEvent $event)
     {
@@ -17,6 +33,12 @@ class ContextPaneEventListener
             $event->getContextPane()->addActions([
                 new Action($this->t("Request site"), 'admin/dashboard/site/request', null, 'globe', 0, true, true),
             ]);
+        } else if ('admin/dashboard/site/' === substr(current_path(), 0, 21)) {
+            if ($site = menu_get_object('ucms_site', 3)) {
+                $event->getContextPane()->addActions(
+                    $this->actionRegistry->getActions($site)
+                );
+            }
         }
     }
 
