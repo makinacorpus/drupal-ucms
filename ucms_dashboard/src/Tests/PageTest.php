f7adbce57d4d5495a800a1e486020e1baf291173
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-05 12:15:39 +0100

    Added unit test skeleton

---
+++ ucms_dashboard/src/Tests/PageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Dashboard\Tests;
+
+use MakinaCorpus\Drupal\Sf\Container\Tests\AbstractDrupalTest;
+use MakinaCorpus\Ucms\Dashboard\Action\ActionRegistry;
+use MakinaCorpus\Ucms\Dashboard\Page\Page;
+
+class PageTest extends AbstractDrupalTest
+{
+    /**
+     * Tests a lot of stuff
+     */
+    public function testEmptyPage()
+    {
+        $formBuilder = $this
+            ->getMockBuilder('\Drupal\Core\Form\FormBuilderInterface')
+            ->getMock()
+        ;
+
+        $actionRegistry = new ActionRegistry();
+
+        $datasource = $this
+            ->getMockBuilder('\MakinaCorpus\Ucms\Dashboard\Page\DatasourceInterface')
+            ->getMock()
+        ;
+
+        $display = $this
+            ->getMockBuilder('\MakinaCorpus\Ucms\Dashboard\Page\DisplayInterface')
+            ->getMock()
+        ;
+
+        $page = new Page($formBuilder, $actionRegistry, $datasource, $display);
+        $render = $page->render([], 'some/path');
+
+        $this->assertArrayHasKey('#theme', $render);
+        $this->assertArrayHasKey('#filters', $render);
+        $this->assertArrayHasKey('#display', $render);
+        $this->assertArrayHasKey('#items', $render);
+        $this->assertArrayHasKey('#pager', $render);
+        $this->assertArrayHasKey('#sort_field', $render);
+        $this->assertArrayHasKey('#sort_order', $render);
+        $this->assertArrayNotHasKey('#search', $render);
+
+        $this->assertEmpty($render['#filters']);
+        $this->assertEmpty($render['#items']);
+    }
+
+    public function testFilterHidesWhenInBaseQuery()
+    {
+        // @todo
+    }
+
+    
+}
