45eec359eda55789e5f9db7bab47c321e3482e7c
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-11 15:06:39 +0100

    contrib: fixes context pane listener to correctly use injected services

--- ucms_contrib/src/EventDispatcher/ContextPaneEventListener.php
+++ ucms_contrib/src/EventDispatcher/ContextPaneEventListener.php
@@ -6,36 +6,57 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 use MakinaCorpus\Ucms\Dashboard\Action\Action;
 use MakinaCorpus\Ucms\Dashboard\EventDispatcher\ContextPaneEvent;
+use MakinaCorpus\Ucms\Layout\Context as LayoutContext;
 
 class ContextPaneEventListener
 {
     use StringTranslationTrait;
 
+    /**
+     * @var LayoutContext
+     */
+    private $layoutContext;
+
+    /**
+     * Default constructor
+     *
+     * @param LayoutContext $layoutContext
+     */
+    public function __construct(LayoutContext $layoutContext)
+    {
+        $this->layoutContext = $layoutContext;
+    }
+
     public function onUcmsdashboardContextinit(ContextPaneEvent $event)
     {
+        $contextPane = $event->getContextPane();
+
         // Add the shopping cart
         if (user_access('use favorites')) {
             // On admin lists, on content creation or on layout edit
             $router_item = menu_get_item();
             $allowed_routes = [
-                'node/%/edit',
+                'node/%',
                 'admin/dashboard/content',
                 'admin/dashboard/media',
             ];
-            if (in_array($router_item['path'], $allowed_routes) || ucms_layout_context_get()->isTemporary()) {
-                ucms_dashboard_context_get()
+            // @todo Inject services
+            if (in_array($router_item['path'], $allowed_routes) || $this->layoutContext->isTemporary()) {
+                $contextPane
                     ->addTab('cart', t("Cart"), 'shopping-cart')
-                    ->add(ucms_contrib_favorite_render(), 'cart');
+                    ->add(ucms_contrib_favorite_render(), 'cart')
+                ;
             }
         }
 
         // Add a backlink
+        //   @todo find a solution for path_is_admin() and current_path()
+        //     maybe bring in the RequestStack
         if (!path_is_admin(current_path())) {
             $backlink = new Action(t("Go to dashboard"), 'admin/dashboard', null, 'dashboard');
         } else {
             $backlink = new Action(t("Go to site"), '<front>', null, 'globe');
         }
-        $contextPane = $event->getContextPane();
         $contextPane->addActions([$backlink]);
 
         // Add node creation link
@@ -43,6 +64,8 @@ class ContextPaneEventListener
             $tab = arg(2);
             $actions = [];
             $types = node_type_get_names();
+            // @todo inject variable contents into a service
+            //   and get rid of variable_get() here
             $tab_types = variable_get('ucms_contrib_tab_'.$tab.'_type', []);
             foreach (array_values($tab_types) as $index => $type) {
                 if (node_access('create', $type)) {
@@ -53,11 +76,11 @@ class ContextPaneEventListener
                         null,
                         $index,
                         !$index,
-                        true);
+                        true
+                    );
                 }
             }
             $contextPane->addActions($actions);
         }
     }
-
 }
