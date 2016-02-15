370f6b9b11f0db128bebd80f4f7f51bc2c3f5f5e
Sebastien Corbin <seb.corbin@gmail.com>
2016-02-15 16:15:18 +0100

    Convert templates to twig

--- ucms_contrib/ucms-contrib-content-result-grid.tpl.php
+++ ucms_contrib/ucms-contrib-content-result-grid.tpl.php
@@ -1,9 +1,6 @@
 <?php foreach ($nodes as $nid => $node): ?>
   <div class="ucms-contrib-result" data-nid="<?php echo $nid; ?>">
-    <?php
-      $view = node_view($node, $view_mode);
-      echo render($view);
-    ?>
+    <?php echo render($view); ?>
     <?php if (isset($actions)): ?>
       <?php echo render($actions); ?>
     <?php endif; ?>
