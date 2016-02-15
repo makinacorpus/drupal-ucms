24813a3f306e1d2325fce43a61adbb56064590c5
Sebastien Corbin <seb.corbin@gmail.com>
2016-02-15 17:44:51 +0100

    Remove all phptemplate files

--- ucms_contrib/ucms-contrib-content-result-grid.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($nodes as $nid => $node): ?>
-  <div class="ucms-contrib-result" data-nid="<?php echo $nid; ?>">
-    <?php echo render($view); ?>
-    <?php if (isset($actions)): ?>
-      <?php echo render($actions); ?>
-    <?php endif; ?>
-  </div>
-<?php endforeach; ?>
--- ucms_layout/ucms-layout-item.tpl.php
+++
@@ -1,3 +0,0 @@
-<div data-nid="<?php echo $nid; ?>" data-viewmode="<?php echo $view_mode; ?>" class="ucms-region-item">
-  <?php echo render($content); ?>
-</div>
