d5bd89ad36b3701165f99f4998552be545c7b3eb
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-16 18:14:47 +0100

    contrib: star/unstar content

---
+++ ucms_contrib/src/Form/NodeStar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Contrib\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class NodeStar extends FormBase
+{
+    /**
+     * {inheritdoc}
+     */
+    public function getFormId()
+    {
+        return 'ucms_contrib_node_star_form';
+    }
+
+    /**
+     * {inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state, $node = null)
+    {
+        $form_state->setTemporaryValue('node', $node);
+
+        return confirm_form([], $this->t("Star %title ?", ['%title' => $node->title]), 'node/' . $node->nid);
+    }
+
+    /**
+     * {inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $node = $form_state->getTemporaryValue('node');
+        $node->is_starred = 1;
+        node_save($node);
+
+        drupal_set_message($this->t("%title has been starred.", ['%title' => $node->title]));
+        $form_state->setRedirect('node/' . $node->nid);
+    }
+}
