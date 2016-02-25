619c0805213bd3efb63a0864ec90314692e968e0
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2016-02-25 19:10:24 +0100

    user: adds an e-mail confirmation field to the account edition form

--- ucms_user/src/Form/AccountEditForm.php
+++ ucms_user/src/Form/AccountEditForm.php
@@ -2,9 +2,12 @@
 
 namespace MakinaCorpus\Ucms\User\Form;
 
+use Drupal\Core\Entity\EntityManager;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class AccountEditForm
@@ -13,6 +16,26 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
 class AccountEditForm extends FormBase
 {
     use StringTranslationTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    static public function create(ContainerInterface $container)
+    {
+        return new self(
+            $container->get('entity.manager')
+        );
+    }
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /**
      * {@inheritdoc}
@@ -42,15 +65,22 @@ class AccountEditForm extends FormBase
             '#default_value' => $account->name,
             '#access' => user_access('change own username', $account),
         );
+
         $form['mail'] = array(
             '#type' => 'textfield',
             '#title' => t('E-mail address'),
             '#maxlength' => EMAIL_MAX_LENGTH,
+            '#attributes' => ['placeholder' => $account->mail],
+        );
+
+        $form['mail_confirmation'] = array(
+            '#type' => 'textfield',
+            '#title' => t('E-mail address confirmation'),
+            '#maxlength' => EMAIL_MAX_LENGTH,
             '#description' => t(
                 'A valid e-mail address. All e-mails from the system will be sent to this address. The e-mail address is not made public and will only be used if you wish to receive a new password or wish to receive certain news or notifications by e-mail.'
             ),
-            '#required' => true,
-            '#default_value' => $account->mail,
+            '#attributes' => ['placeholder' => 'Confirm the new e-mail address'],
         );
 
         $form['actions'] = array('#type' => 'actions');
@@ -90,27 +120,38 @@ class AccountEditForm extends FormBase
             }
         }
 
-        // Trim whitespace from mail, to prevent confusing 'e-mail not valid'
-        // warnings often caused by cutting and pasting.
-        $new_mail = $form_state->getValue('mail');
-        $new_mail = trim($new_mail);
-        $form_state->setValue('mail', $new_mail);
+        if ($new_mail = $form_state->getValue('mail')) {
+            // Trim whitespace from mail, to prevent confusing 'e-mail not valid'
+            // warnings often caused by cutting and pasting.
+            $new_mail = trim($new_mail);
+            $form_state->setValue('mail', $new_mail);
 
-        // Validate the e-mail address, and check if it is taken by an existing user.
-        if ($error = user_validate_mail($new_mail)) {
-            $form_state->setErrorByName('mail', $error);
-        } elseif ((bool)db_select('users')
-            ->fields('users', array('uid'))
-            ->condition('uid', $account->uid, '<>')
-            ->condition('mail', db_like($new_mail), 'LIKE')
-            ->range(0, 1)
-            ->execute()
-            ->fetchField()
-        ) {
-            form_set_error(
-                'mail',
-                t('The e-mail address %email is already taken.', array('%email' => $new_mail))
-            );
+            $mail_confirmation = trim($form_state->getValue('mail_confirmation'));
+
+            // Validate the e-mail address, and check if it is taken by an existing user.
+            if (!valid_email_address($new_mail)) {
+                form_set_error(
+                    'mail',
+                    t('The e-mail address %mail is not valid.', array('%mail' => $new_mail))
+                );
+            } elseif ($new_mail !== $mail_confirmation) {
+                form_set_error(
+                    'mail_confirmation',
+                    t("The e-mail's confirmation doesn't match with the first supplied e-mail.")
+                );
+            } elseif ((bool)db_select('users')
+                ->fields('users', array('uid'))
+                ->condition('uid', $account->uid, '<>')
+                ->condition('mail', db_like($new_mail), 'LIKE')
+                ->range(0, 1)
+                ->execute()
+                ->fetchField()
+            ) {
+                form_set_error(
+                    'mail',
+                    t('The e-mail address %email is already taken.', array('%email' => $new_mail))
+                );
+            }
         }
 
     }
@@ -123,13 +164,13 @@ class AccountEditForm extends FormBase
     {
         $account = $form_state->getTemporaryValue('account');
 
-        user_save(
-            $account,
-            [
-                'name' => $form_state->getValue('name'),
-                'mail' => $form_state->getValue('mail'),
-            ]
-        );
+        $account->setUsername($form_state->getValue('name'));
+        if ($new_mail = $form_state->getValue('mail')) {
+            $account->setEmail($new_mail);
+        }
+
+        $this->entityManager->getStorage('user')->save($account);
+
         // Clear the page cache because pages can contain usernames and/or profile information:
         cache_clear_all();
         drupal_set_message($this->t("Your account information has been updated."));
