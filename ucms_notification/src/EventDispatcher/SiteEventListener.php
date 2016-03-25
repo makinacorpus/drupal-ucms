fe9d8b37a4652177d63737860e81899f915a8158
Sebastien Corbin <seb.corbin@gmail.com>
2016-03-25 17:16:47 +0100

    Add site users to site channel

---
+++ ucms_notification/src/EventDispatcher/SiteEventListener.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Notification\EventDispatcher;
+
+use MakinaCorpus\Ucms\Notification\NotificationService;
+use MakinaCorpus\Ucms\Site\EventDispatcher\SiteEvent;
+
+class SiteEventListener
+{
+    /**
+     * @var NotificationService
+     */
+    private $service;
+
+    /**
+     * Default constructor
+     *
+     * @param NotificationService $notificationService
+     */
+    public function __construct(NotificationService $notificationService)
+    {
+        $this->service = $notificationService;
+    }
+
+    /**
+     * Event: On adding a site.
+     *
+     * @param SiteEvent $event
+     */
+    public function onSiteCreate(SiteEvent $event)
+    {
+        $site = $event->getSite();
+        $this->service->subscribe($site->uid, 'site:'.$site->getId());
+    }
+    /**
+     * Event: On adding an user to a site.
+     *
+     * @param SiteEvent $event
+     */
+    public function onSiteWebmasteraddnew(SiteEvent $event)
+    {
+        $site = $event->getSite();
+        $uid = $event->getArgument('webmaster_id');
+        $this->service->subscribe($uid, 'site:'.$site->getId());
+    }
+
+    /**
+     * Event: On adding an user to a site.
+     *
+     * @param SiteEvent $event
+     */
+    public function onSiteWebmasteraddexisting(SiteEvent $event)
+    {
+        $this->onSiteWebmasteraddnew($event);
+    }
+
+    /**
+     * Event: On deleting an user to a site.
+     *
+     * @param SiteEvent $event
+     */
+    public function onSiteWebmasterdelete(SiteEvent $event)
+    {
+        $site = $event->getSite();
+        $uid = $event->getArgument('webmaster_id');
+        $this->service->deleteSubscriptionsFor($uid, ['site:'.$site->getId()]);
+    }
+}
