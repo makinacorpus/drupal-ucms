d07680326c85ae4b3cf68ba8503f4fc0ec53fa7c
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2016-03-30 12:05:25 +0200

    notification: fixes the notification compiler pass to use a tag instead of a hard coded list of service identifiers

--- ucms_notification/src/DependencyInjection/Compiler/NotificationCompilerPass.php
+++ ucms_notification/src/DependencyInjection/Compiler/NotificationCompilerPass.php
@@ -8,7 +8,6 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class NotificationCompilerPass implements CompilerPassInterface
 {
-
     /**
      * You can modify the container here before it is dumped to PHP code.
      *
@@ -17,13 +16,10 @@ class NotificationCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if ($container->hasDefinition('ucms_contrib.type_handler')) {
-            $definitions = [
-                'ucms_notification.content_add',
-                'ucms_notification.content_edit',
-                'ucms_notification.content_delete',
-            ];
-            foreach ($definitions as $definition) {
-                $formatter = $container->getDefinition($definition);
+            $taggedServices = $container->findTaggedServiceIds('ucms_contrib.type_handler');
+
+            foreach ($taggedServices as $id => $attributes) {
+                $formatter = $container->getDefinition($id);
 
                 $formatter->addMethodCall(
                     'setTypeHandler',
