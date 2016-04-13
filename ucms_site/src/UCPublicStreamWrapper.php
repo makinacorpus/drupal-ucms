8ff48ee67641f5ea6ea630364c8078fd66ed45a9
regilero <regis.leroy@makina-corpus.com>
2016-04-13 17:42:13 +0200

    after code review: remove drupal_static indirections

--- ucms_site/src/UCPublicStreamWrapper.php
+++ ucms_site/src/UCPublicStreamWrapper.php
@@ -18,10 +18,7 @@ class UCPublicStreamWrapper extends \DrupalPublicStreamWrapper
      * May set it on a CDN domain if asked to do so.
      */
     function getExternalUrl() {
-        $use_cdn =& drupal_static('UCPublicStreamWrapper_use_cdn');
-        if (!isset($use_cdn)) {
-            $use_cdn = variable_get('ucms_site_use_cdn', False);
-        }
+        $use_cdn = variable_get('ucms_site_use_cdn', False);
 
         if ($use_cdn) {
 
