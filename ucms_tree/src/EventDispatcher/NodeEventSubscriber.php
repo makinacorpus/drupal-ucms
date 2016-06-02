1b3e00db3334ef92d84d949e7d83a30fe1b748fd
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-06-02 17:54:20 +0200

    tree, site: #1013 - when cloning a content, update site menu links accordingly

---
+++ ucms_tree/src/EventDispatcher/NodeEventSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Tree\EventDispatcher;
+
+use MakinaCorpus\Ucms\Contrib\EventDispatcher\NodeEvent;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class NodeEventSubscriber implements EventSubscriberInterface
+{
+    private $db;
+
+    static public function getSubscribedEvents()
+    {
+        return [
+            NodeEvent::EVENT_INSERT => [
+                ['onInsert', 0],
+            ],
+        ];
+    }
+
+    public function __construct(\DatabaseConnection $db)
+    {
+        $this->db = $db;
+    }
+
+    public function onInsert(NodeEvent $event)
+    {
+        $node = $event->getNode();
+
+        // When inserting a node, site_id is always the current site context.
+        // Menu items that points to the original node within the site should
+        // be replaced by menu items pointing to the new element. Instead of
+        // polluting caches, we are just going to replace linked paths rather
+        // seamlessly using a single SQL query, then silently drop menu items
+        // cache (instead of deleting/saving new elements then clearing the
+        // whole menu cache).
+        if ($event->isClone() && $node->site_id) {
+            $this
+                ->db
+                ->query(
+                    "
+                        UPDATE {menu_links} ml
+                        JOIN {umenu} u ON u.name = ml.menu_name
+                        SET link_path = :route
+                        WHERE u.site_id = :site AND ml.link_path = :legacy
+                    ",
+                    [
+                        ':route'  => 'node/' . $node->nid,
+                        ':site'   => $node->site_id,
+                        ':legacy' => 'node/' . $node->parent_nid,
+                    ]
+                )
+            ;
+
+            // @todo Hardcoded procedural call, this is so wrong
+            menu_cache_clear_all();
+        }
+    }
+}
