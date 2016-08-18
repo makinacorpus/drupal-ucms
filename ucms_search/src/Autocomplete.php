8977c27f9e99889c76c2c2cecaf70191b92612a3
Sebastien Corbin <seb.corbin@gmail.com>
2016-08-18 18:48:31 +0200

    Fix #1357 Autocompletion should be aware of site context

--- ucms_search/src/Autocomplete.php
+++ ucms_search/src/Autocomplete.php
@@ -3,6 +3,7 @@
 namespace MakinaCorpus\Ucms\Search;
 
 use Elasticsearch\Client;
+use MakinaCorpus\Ucms\Site\SiteManager;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
@@ -22,15 +23,20 @@ class Autocomplete
      * @var string
      */
     protected $index;
+    /**
+     * @var \MakinaCorpus\Ucms\Site\SiteManager
+     */
+    private $siteManager;
 
     /**
      * @param \Elasticsearch\Client $client
      */
-    public function __construct(Client $client, IndexStorage $storage)
+    public function __construct(Client $client, IndexStorage $storage, SiteManager $siteManager)
     {
         $this->client = $client;
         $this->storage = $storage;
         $this->index = $this->storage->getIndexRealname('private');
+        $this->siteManager = $siteManager;
     }
 
     /**
@@ -44,25 +50,31 @@ class Autocomplete
      *   Which fuzziness to apply. For example, with fuzziness of 1, the text
      *   'paage' will match with term 'page'.
      *   Default to none (0).
-     * 
+     *
      * @return JsonResponse
      */
     public function execute($text, $fuzziness = 0, $field = 'autocomplete')
     {
         $params = [
-          'index' => $this->index,
-          'body' => [
-            'autocomplete-suggest' => [
-              'text' => $text,
-              'completion' => [
-                'field' => $field,
-                'fuzzy' => [
-                  'fuzziness' => $fuzziness,
+            'index' => $this->index,
+            'body'  => [
+                'autocomplete-suggest' => [
+                    'text'       => $text,
+                    'completion' => [
+                        'field'   => $field,
+                        'fuzzy'   => [
+                            'fuzziness' => $fuzziness,
+                        ],
+                    ],
                 ],
-              ],
             ],
-          ],
         ];
+        if ($this->siteManager->hasContext()) {
+            $params['body']['autocomplete-suggest']['completion']['context'] = [
+                'site_id' => $this->siteManager->getContext()->getId(),
+            ];
+        }
+
         $result = $this->client->suggest($params);
 
         $suggestions = [];
