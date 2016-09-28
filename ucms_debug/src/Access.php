6a576f7af345c0ea2bd2366dcd0c3da39335b249
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-09-28 11:12:24 +0200

    debug: initial commit of the debug helper module

---
+++ ucms_debug/src/Access.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MakinaCorpus\Ucms\Debug;
+
+final class Access
+{
+    const PERM_ACCESS_DEBUG = 'ucms debug access';
+}
--- ucms_user/src/Controller/AccountController.php
+++ ucms_user/src/Controller/AccountController.php
@@ -23,34 +23,6 @@ class AccountController extends Controller
     }
 
     /**
-     * @return AccountInterface
-     */
-    private function getCurrentUser()
-    {
-        return $this->get('current_user');
-    }
-
-    /**
-     * Debug screen that displays current user grants.
-     */
-    public function viewGrantsAction(AccountInterface $account)
-    {
-        if (!$account->isAuthenticated() || $account->id() !== $this->getCurrentUser()->id()) {
-            throw $this->createAccessDeniedException();
-        }
-
-        $table = $this->createAdminTable('user_grants');
-
-        $table->addHeader($this->t("Grants"), 'basic');
-
-        foreach (node_access_grants('view', $account) as $realm => $gids) {
-            $table->addRow($realm, implode(', ', $gids));
-        }
-
-        return $table->render();
-    }
-
-    /**
      * User details display
      */
     public function viewInfoAction(AccountInterface $account)
