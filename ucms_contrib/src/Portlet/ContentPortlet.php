cc8241ea9a21e16ba2c1bf94e5c69e2503ba3aa2
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-07-02 13:13:09 +0200

    contrib: restored portlets

--- ucms_contrib/src/Portlet/ContentPortlet.php
+++ ucms_contrib/src/Portlet/ContentPortlet.php
@@ -2,57 +2,21 @@
 
 namespace MakinaCorpus\Ucms\Contrib\Portlet;
 
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-use MakinaCorpus\Calista\Action\ActionProviderInterface;
-use MakinaCorpus\Drupal\Dashboard\Page\DatasourceInterface;
-use MakinaCorpus\Drupal\Dashboard\Page\PageBuilder;
-use MakinaCorpus\Drupal\Dashboard\Portlet\AbstractPortlet;
+use MakinaCorpus\Drupal\Calista\Portlet\AbstractPortlet;
 use MakinaCorpus\Ucms\Contrib\TypeHandler;
-use MakinaCorpus\Ucms\Site\SiteManager;
 
 class ContentPortlet extends AbstractPortlet
 {
-    use StringTranslationTrait;
-
-    /**
-     * @var DatasourceInterface
-     */
-    private $datasource;
-
-    /**
-     * @var ActionProviderInterface
-     */
-    private $actionProvider;
-
-    /**
-     * @var TypeHandler
-     */
     private $typeHandler;
-
-    /**
-     * @var SiteManager
-     */
-    private $siteManager;
 
     /**
      * Default constructor
      *
-     * @param DatasourceInterface $datasource
-     * @param ActionProviderInterface $actionProvider
      * @param TypeHandler $typeHandler
-     * @param SiteManager $siteManager
      */
-    public function __construct(
-        DatasourceInterface $datasource,
-        ActionProviderInterface $actionProvider,
-        TypeHandler $typeHandler,
-        SiteManager $siteManager
-    ) {
-        $this->datasource = $datasource;
-        $this->actionProvider = $actionProvider;
+    public function __construct(TypeHandler $typeHandler)
+    {
         $this->typeHandler = $typeHandler;
-        $this->siteManager = $siteManager;
     }
 
     /**
@@ -66,11 +30,8 @@ class ContentPortlet extends AbstractPortlet
     /**
      * {@inheritDoc}
      */
-    public function getPath()
+    public function getRoute()
     {
-        if ($this->siteManager->getAccess()->userIsWebmaster($this->getAccount())) {
-            return 'admin/dashboard/content/local';
-        }
         return 'admin/dashboard/content';
     }
 
@@ -79,7 +40,6 @@ class ContentPortlet extends AbstractPortlet
      */
     public function getActions()
     {
-        return []; // FIXME
         return array_merge(
             $this->actionProvider->getActions('editorial'),
             $this->actionProvider->getActions('component')
@@ -89,39 +49,14 @@ class ContentPortlet extends AbstractPortlet
     /**
      * {@inheritdoc}
      */
-    protected function createPage(PageBuilder $pageBuilder)
+    public function getContent()
     {
-        $query = [];
-        $query['type'] = $this->typeHandler->getEditorialContentTypes();
-        $query['is_global'] = 0;
-
-        $currentUser = $this->getAccount();
-
-        // Only for webmaster, show local nodes (instead of own).
-        if ($this->siteManager->getAccess()->userIsWebmaster($currentUser)) {
-            $map = $this->siteManager->getAccess()->getUserRoles($currentUser);
-            $site_ids = [];
-            foreach ($map as $item) {
-                $site_ids[] = $item->getSiteId();
-            }
-
-            if ($site_ids) {
-                $query['site_id'] = $site_ids;
-            }
-        }
-
-        $pageBuilder
-            ->setDatasource($this->datasource)
-            ->setAllowedTemplates(['table' => 'module:ucms_contrib:Portlet/page-node-portlet.html.twig'])
-            ->setBaseQuery($query)
-        ;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function userIsAllowed(AccountInterface $account)
-    {
-        return true;
+        return $this->renderPage(
+            'ucms_contrib.datasource.node',
+            'module:ucms_contrib:views/Portlet/page-node-portlet.html.twig',
+            [
+                'type' => $this->typeHandler->getContentTypes(),
+            ]
+        );
     }
 }
--- ucms_contrib/src/Portlet/MediaPortlet.php
+++ ucms_contrib/src/Portlet/MediaPortlet.php
@@ -2,57 +2,21 @@
 
 namespace MakinaCorpus\Ucms\Contrib\Portlet;
 
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-use MakinaCorpus\Calista\Action\ActionProviderInterface;
-use MakinaCorpus\Drupal\Dashboard\Page\DatasourceInterface;
-use MakinaCorpus\Drupal\Dashboard\Page\PageBuilder;
-use MakinaCorpus\Drupal\Dashboard\Portlet\AbstractPortlet;
+use MakinaCorpus\Drupal\Calista\Portlet\AbstractPortlet;
 use MakinaCorpus\Ucms\Contrib\TypeHandler;
-use MakinaCorpus\Ucms\Site\SiteManager;
 
 class MediaPortlet extends AbstractPortlet
 {
-    use StringTranslationTrait;
-
-    /**
-     * @var DatasourceInterface
-     */
-    private $datasource;
-
-    /**
-     * @var ActionProviderInterface
-     */
-    private $actionProvider;
-
-    /**
-     * @var TypeHandler
-     */
     private $typeHandler;
-
-    /**
-     * @var SiteManager
-     */
-    private $siteManager;
 
     /**
      * Default constructor
      *
-     * @param DatasourceInterface $datasource
-     * @param ActionProviderInterface $actionProvider
      * @param TypeHandler $typeHandler
-     * @param SiteManager $siteManager
      */
-    public function __construct(
-        DatasourceInterface $datasource,
-        ActionProviderInterface $actionProvider,
-        TypeHandler $typeHandler,
-        SiteManager $siteManager
-    ) {
-        $this->datasource = $datasource;
-        $this->actionProvider = $actionProvider;
+    public function __construct(TypeHandler $typeHandler)
+    {
         $this->typeHandler = $typeHandler;
-        $this->siteManager = $siteManager;
     }
 
     /**
@@ -66,11 +30,8 @@ class MediaPortlet extends AbstractPortlet
     /**
      * {@inheritDoc}
      */
-    public function getPath()
+    public function getRoute()
     {
-        if ($this->siteManager->getAccess()->userIsWebmaster($this->getAccount())) {
-            return 'admin/dashboard/media/local';
-        }
         return 'admin/dashboard/media';
     }
 
@@ -79,46 +40,20 @@ class MediaPortlet extends AbstractPortlet
      */
     public function getActions()
     {
-        return []; // FIXME
         return $this->actionProvider->getActions('media');
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
-    protected function createPage(PageBuilder $pageBuilder)
+    public function getContent()
     {
-        $query = [];
-        $query['type'] = $this->typeHandler->getMediaTypes();
-        $query['is_global'] = 0;
-
-        $currentUser = $this->getAccount();
-
-        // Only for webmaster, show local nodes (instead of own).
-        if ($this->siteManager->getAccess()->userIsWebmaster($currentUser)) {
-            $map = $this->siteManager->getAccess()->getUserRoles($currentUser);
-            $site_ids = [];
-            foreach ($map as $item) {
-                $site_ids[] = $item->getSiteId();
-            }
-
-            if ($site_ids) {
-                $query['site_id'] = $site_ids;
-            }
-        }
-
-        $pageBuilder
-            ->setDatasource($this->datasource)
-            ->setAllowedTemplates(['table' => 'module:ucms_contrib:Portlet/page-node-portlet.html.twig'])
-            ->setBaseQuery($query)
-        ;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function userIsAllowed(AccountInterface $account)
-    {
-        return true;
+        return $this->renderPage(
+            'ucms_contrib.datasource.node',
+            'module:ucms_contrib:views/Portlet/page-node-portlet.html.twig',
+            [
+                'type' => $this->typeHandler->getMediaTypes(),
+            ]
+        );
     }
 }
