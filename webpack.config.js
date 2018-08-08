2bb9b5fc1cb861fd57da6607e90df678ce699abd
Pierre Rineau <pierre.rineau@makina-corpus.com>
2018-08-08 13:56:55 +0200

    all: modern front asset tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,70 @@
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const path = require('path');
 const webpack = require('webpack');
 
-const SRC_DASHBOARD = path.resolve(__dirname, 'ucms_dashboard/src-front');
-// const DIST_DASHBOARD = path.resolve(__dirname, 'ucms_dashboard/dist');
-const extractLess = new ExtractTextPlugin({filename: "[name]/dist/style.css"});
+module.exports = (env, argv) => {
+  const SRC_DASHBOARD = path.resolve(__dirname, 'ucms_dashboard/src-front');
 
-module.exports = {
-  plugins: [
-    // new CleanWebpackPlugin([DIST_DASHBOARD]),
-    // new MinifyPlugin(),
-    extractLess
-  ],
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: "babel-loader"
-      }, {
-        loader: "ts-loader"
-      }],
-    },{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: "babel-loader"
-      }]
-    },{
-      test: /\.less$/,
-      use: extractLess.extract({
-        fallback: "style-loader",
-        use: [{
-          loader: "css-loader",
-          options: {
-            minimize: false, // true,
-            url: false
-          }
-        },{
-          loader: "less-loader",
-          options: {
-            relativeUrls: true
-          }
-        }]
-      })
-    }]
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  externals: {
-    "jquery": "jQuery",
-    "react": "React",
-    "react-dom": "ReactDOM"
-  },
-  entry: {
-    /*'core-js/modules/es6.promise', */
-    'ucms_dashboard': [
-      /* 'babel-polyfill', */
-      SRC_DASHBOARD + '/seven-fixes.less'
-    ]
-  },
-  output: {
-    filename: '[name]/dist/script.js'
+  const plugins = [
+    new CleanWebpackPlugin([
+      path.resolve(__dirname, 'polyfill/dist'),
+      path.resolve(__dirname, 'ucms_dashboard/dist'),
+    ]),
+    new MiniCssExtractPlugin({filename: "[name]/dist/style.css"})
+  ];
+
+  if ('production' === argv.mode) {
+    plugins.push(new MinifyPlugin());
   }
+
+  return {
+    devtool: ('production' === argv.mode ? false : 'source-map'),
+    plugins: plugins,
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "babel-loader"
+        }, {
+          loader: "ts-loader"
+        }],
+      },{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "babel-loader"
+        }]
+      },{
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader"
+        ]
+      }]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    externals: {
+      "jquery": "jQuery",
+      "react": "React",
+      "react-dom": "ReactDOM"
+    },
+    entry: {
+      /* polyfill: [
+        'core-js/modules/es6.promise',
+      ], */
+      'ucms_dashboard': [
+        SRC_DASHBOARD + '/seven-fixes.less'
+      ]
+    },
+    output: {
+      path: __dirname,
+      filename: '[name]/dist/script.js'
+    }
+  };
 };
