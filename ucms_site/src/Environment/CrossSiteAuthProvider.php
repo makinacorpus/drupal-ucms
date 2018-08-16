6dfa89c215af5afb58483100f1ad2bf5a10fa21a
Pierre Rineau <pierre.rineau@makina-corpus.com>
2018-08-16 18:55:29 +0200

    site: sso security improvement

--- ucms_site/src/Environment/CrossSiteAuthProvider.php
+++ ucms_site/src/Environment/CrossSiteAuthProvider.php
@@ -48,27 +48,27 @@ class CrossSiteAuthProvider implements AuthenticationProviderInterface
     public function authenticate(Request $request)
     {
         if (!$token = $request->query->get(self::TOKEN_PARAMETER)) {
-            return null;
+            return;
         }
 
         // We should not have to do this since the applies() method
         // has in theory be called prior to us, but better be safe
         // than sorry, context could have changed in so many ways.
         if (!$this->siteManager->hasContext()) {
-            return null;
+            return;
         }
 
         $siteId = $this->siteManager->getContext()->getId();
         $authToken = $this->authTokenStorage->find($siteId, $token);
 
-        if ($authToken->isValid($token)) {
-            return $this->entityTypeManager->getStorage('user')->load($authToken->getUserId());
-        }
+        if ($userId = $authToken->getUserId()) {
+            // Valid or invalid, no matter, prevent the same token from being
+            // used more than once for the same login session.
+            $this->authTokenStorage->delete($authToken->getSiteId(), $userId);
 
-        // Opportunistic token deletion, this will help lowering
-        // database table row volume. Notice it may delete a non
-        // loaded instance which have a different token for the
-        // same site.
-        $this->authTokenStorage->delete($authToken->getSiteId(), $authToken->getUserId());
+            if ($authToken->isValid($token)) {
+                return $this->entityTypeManager->getStorage('user')->load($authToken->getUserId());
+            }
+        }
     }
 }
